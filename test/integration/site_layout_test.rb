925314f32a46ec3c2c8cdc5aae948b5e32e807d7
Miyauchi <hy.miyauchi@gmail.com>
2022-06-12 17:11:01 +0900

    Finish user edit, update, index, and destroy actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,16 +2,48 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+
+  test "layout links without login" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get help_path
+    assert_select "title", full_title("Help")
+    get login_path
+    assert_select "title", full_title("Log in")
+    get about_path
+    assert_select "title", full_title("About")
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
   end
+
+  test "layout links with login" do
+    log_in_as(@user)
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    get help_path
+    assert_select "title", full_title("Help")
+    get users_path
+    assert_select "title", full_title("All users")
+    get about_path
+    assert_select "title", full_title("About")
+    get contact_path
+    assert_select "title", full_title("Contact")
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
 end
